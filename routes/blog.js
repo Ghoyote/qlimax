5b8c0bb62350ae5100bdf59500df237f4cde51cd
Kowus <barnabas@elite-education.org>
2017-01-27 18:30:19 +0000

    init

---
+++ routes/blog.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+	res.render('blog', {
+		title: process.env.TITLE_QLIMAX,
+		blog: true
+	});
+});
+
+module.exports = router;
