5de1d4b20e30391a4d667e825522be60cd786df3
Kowus <barnabas@elite-education.org>
2017-02-28 18:02:50 +0000

    added appcache

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -58,32 +58,6 @@ var appCache = window.applicationCache;
 
 appCache.update();
 
-/*
-switch (appCache.status) {
-	case appCache.UNCACHED: // UNCACHED == 0
-		return 'UNCACHED';
-		break;
-	case appCache.IDLE: // IDLE == 1
-		return 'IDLE';
-		break;
-	case appCache.CHECKING: // CHECKING == 2
-		return 'CHECKING';
-		break;
-	case appCache.DOWNLOADING: // DOWNLOADING == 3
-		return 'DOWNLOADING';
-		break;
-	case appCache.UPDATEREADY:  // UPDATEREADY == 4
-		return 'UPDATEREADY';
-		break;
-	case appCache.OBSOLETE: // OBSOLETE == 5
-		return 'OBSOLETE';
-		break;
-	default:
-		return 'UKNOWN CACHE STATUS';
-		break;
-};
-*/
-
 
 
 window.addEventListener('load', function(e) {
