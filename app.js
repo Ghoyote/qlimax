659f6be3a4f8aba93cfbf4625547607f6ef70f02
Kowus <barnabas@elite-education.org>
2017-02-28 14:34:28 +0000

    singled out portfolio retouched partners, corrected makeup text

--- app.js
+++ app.js
@@ -30,9 +30,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
-app.use('/blog', blog);
-app.use('/admin',admin);
+// app.use('/users', users);
+// app.use('/blog', blog);
+// app.use('/admin',admin);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
